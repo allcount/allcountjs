db03bd00a4f17b318bb4847d286b3e81c2151bb1
Pavel Tiunov <pavel.tiunov@gmail.com>
2015-11-19 12:43:24 +0300

    Add logging to figure out why travis build stalled

--- allcount-server-startup.js
+++ allcount-server-startup.js
@@ -37,13 +37,16 @@ module.exports = function (appService, gitRepoUrl, proxyHandler, injection, http
         },
         stop: function () {
             var defer = Q.defer();
+            console.log("Shutting down HTTP server...");
             this.server.close(function () {
-                console.log('Application server for "' + gitRepoUrl + '" on port ' + app.get('port') + ' has stopped');
                 defer.resolve(null);
             });
             return defer.promise.then(function () {
+                console.log("Closing db connection...");
                 return storageDriver.closeConnection();
-            });
+            }).then(function () {
+                console.log('Application server for "' + gitRepoUrl + '" on port ' + app.get('port') + ' has stopped');
+            })
         }
     }
 };
