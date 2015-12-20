94d400e613d482be31cdbc8421aa4e11998379bb
Georgy Chikhladze <georgiy.chikhladze@gmail.com>
2015-12-20 14:28:08 +0300

    WIP on #66: do not show the forgot password link when not configured

--- services/forgot-password-service.js
+++ services/forgot-password-service.js
@@ -1,9 +1,14 @@
-module.exports = function () {
+var _ = require('lodash');
+
+module.exports = function (injection) {
+    console.log('FPS init');
+
     var service = {};
 
     service.config = {};
 
     service.compile = function (objects) {
+        console.log('compilation');
         objects.forEach(function (obj) {
             var config = obj.propertyValue('forgotPassword');
             if (config) {
@@ -11,6 +16,26 @@ module.exports = function () {
             }
         });
     };
+
+    injection.overrideFactory('templateVarService', 'templateVarServiceBase', function (templateVarServiceBase) {
+        console.log('init');
+        var service = templateVarServiceBase;
+        var parentVars = service.vars;
+        service.vars = function (req, obj) {
+            console.log('vars');
+            var vars = _.extend(parentVars(req, obj), {
+                forgotPasswordConfig: service.config
+            });
+            console.log(JSON.stringify(parentVars))
+            console.log(JSON.stringify(vars))
+            return vars;
+        };
+      /*  service.setupLocals = function (req, res, obj) {
+            console.log('setupLocals');
+            res.locals = _.extend(res.locals, service.vars(req, obj));
+        }; //todo: get rid of it (copy-pasted)*/
+        return service;
+    });
 
     return service;
 };
--- services/template-var-service.js
+++ services/template-var-service.js
@@ -1,9 +1,11 @@
 var _ = require('underscore');
 
 module.exports = function (menuService, messagesService, securityService, themeService, entityDescriptionService, trackingService, jadeParserService, securityConfigService, integrationService, assetsService) {
+    console.log('orig init');
     var service = {};
 
     service.vars = function (req, obj) {
+        console.log('orig vars');
         var result = obj || {};
         var acceptLanguageHeader = req.header('Accept-Language');
         var languageSetting = req.languageSetting;
@@ -34,6 +36,7 @@ module.exports = function (menuService, messagesService, securityService, themeS
     };
 
     service.setupLocals = function (req, res, obj) {
+        console.log('orig setupLocals');
         res.locals = _.extend(res.locals, service.vars(req, obj));
     };
 
